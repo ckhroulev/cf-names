2f312394841ab97c4c09386adc60f9b58b31729f
Constantine Khroulev <ckhroulev@alaska.edu>
2017-01-13 13:16:00 -0600

    Some cleanup.

--- cf-standard-names.el
+++ cf-standard-names.el
@@ -1,6 +1,7 @@
 ;; -*- lexical-binding: t -*-
 (require 'helm)
 (require 'dom)
+(require 'cl)
 
 (defcustom cf-standard-names-table-filename
   "~/github/ckhroulev/helm-cf-standard-names/cf-standard-name-table.xml.gz"
@@ -13,32 +14,28 @@
     (mapcar (lambda (entry)
               (let ((id (dom-attr entry 'id))
                     (units (dom-text (dom-by-tag entry 'canonical_units)))
-                    (doc (dom-text (dom-by-tag entry 'description)))
-                    (grib (dom-text (dom-by-tag entry 'grib)))
-                    (amip (dom-text (dom-by-tag entry 'amip))))
+                    (doc   (dom-text (dom-by-tag entry 'description)))
+                    (grib  (dom-text (dom-by-tag entry 'grib)))
+                    (amip  (dom-text (dom-by-tag entry 'amip))))
                 (list (format "%s (%s)" id units)
                       id units doc grib amip)))
             (dom-by-tag xml 'entry))))
 
-(defvar cf-standard-names-table nil)
+(defvar cf-standard-names nil)
 
 (defun cf-standard-names ()
-  (or cf-standard-names-table
-      (setf cf-standard-names-table (cf-standard-names-parse))))
+  (or cf-standard-names
+      (setf cf-standard-names (cf-standard-names-parse))))
 
 (defun cf-standard-names-display-entry (data)
   "Display an entry of the CF standard names table.
 `data' is a list consisting of (name units description grib amip)."
-  (let ((id     (nth 0 data))
-        (units  (nth 1 data))
-        (doc    (nth 2 data))
-        (grib   (nth 3 data))
-        (amip   (nth 4 data))
-        (buffer (get-buffer-create "*cf-standard-names-info*"))
-        (inhibit-read-only t))
+  (multiple-value-bind (id units doc grib amip) data
+   (let ((buffer (get-buffer-create "*cf-standard-names-info*"))
+         (inhibit-read-only t))
     (with-current-buffer buffer
       (erase-buffer)
-      (display-buffer buffer)           ; shr uses window info to render, so we need to display it
+      (display-buffer buffer) ; shr uses window info to render, so we need to display it
       (shr-insert-document
        `(html ()
               (body ()
@@ -47,11 +44,11 @@
                            (tr () (td () (b () "Units"))       (td () ,units))
                            (tr () (td () (b () "Description")) (td () ,doc))
                            (tr () (td () (b () "GRIB"))
-                               (td () ,(if (string= grib "") "---" grib)))
+                               (td () ,(if (string= grib "") "empty" grib)))
                            (tr () (td () (b () "AMIP"))
-                               (td () ,(if (string= amip "") "---" amip)))
+                               (td () ,(if (string= amip "") "empty" amip)))
                            ))))
-      (view-mode nil))))
+      (view-mode nil)))))
 
 (defvar cf-standard-names-source
   (helm-build-sync-source "CF standard names"
